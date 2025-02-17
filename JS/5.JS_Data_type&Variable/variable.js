938fc34c3e788fb926786f663e51816dc211d67f
devJiraynor <jiraynor@naver.com>
2025-02-17 11:49:50 +0900

    2025.02.17. 11:50

--- JS/5.JS_Data_type&Variable/variable.js
+++ JS/5.JS_Data_type&Variable/variable.js
@@ -1,20 +1,33 @@
 /*
-    변수 : 
-    - 
+    변수 : 데이터를 저장하는 공간에 이름을 부여한 것
+    - var, let, const 키워드를 사용하여 변수를 선언할 수 있음
+    - javascript는 변수의 타입이 존재하지 않음
 */
-
+var varVariable = 10;
+let letVariable = 10;
+const constVariable = 10;
 
 
 /*
-    변수의 중복 선언 : 
+    변수의 중복 선언 : var 키워드를 사용하여 선언한 변수는 중복 선언이 가능
 */
+var age = 20;
+var age = 10;
 
+// let으로 선언한 변수는 중복 선언이 불가능
+let address = '부산광역시';
+// let address = '울산광역시';
 
 
 /*
-    동적 타이핑 : 
+    동적 타이핑 : 변수의 타입이 데이터가 할당되는 순간에 결정됨
 */
-
+var variable;
+console.log(typeof variable);
+variable = '안녕';
+console.log(typeof variable);
+variable = 10;
+console.log(typeof variable);
 
 
 /*
